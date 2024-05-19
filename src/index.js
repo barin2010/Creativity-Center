98fc0886dc7d767327dadad98808636fd94a3aec
barin2010 <uyrirudenko@gmail.com>
2024-05-19 18:59:39 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import '../src/styles/index.css';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/Creativity-Center">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
