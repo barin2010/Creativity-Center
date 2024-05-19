9455191e3e249c6f0b5bd27d51272bc44816bdc8
barin2010 <uyrirudenko@gmail.com>
2024-05-19 23:39:34 +0300

    Update Organizers.jsx

--- src/components/organizers/Organizers.jsx
+++ src/components/organizers/Organizers.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import css from './Organizers.module.css';
 
 const Organizers = () => {
-  return <div>Organizers</div>;
+  return <div className={css.organizers}>Organizers</div>;
 };
 
 export default Organizers;
